26c105297ce3d047eb4498959158f47daeef8853
niubingru <1470699099@qq.com>
2017-05-23 10:21:11 +0800

    n

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,83 @@
     <template>
-        <div>
-            home
+    	<div>
+	    	<header class="p_header">
+				<div class="h_txt">
+					<div class="pageback">
+						<a href="javascript:window.history.back(-1);"></a>
+					</div>
+					<div class="h_h2">
+						<h2>
+							<i>
+							</i>
+							登录
+						</h2>
+					</div>
+					<div class="home">
+						<a href="/wap/index.html">
+						</a>
+					</div>
+					<div class="myuser">
+						<a href="/wap/user/index.html">
+						</a>
+					</div>
+					<div class="carth">
+						<a href="/wap/flow.html">
+						</a>
+					</div>
+				</div>
+			</header>
+	    	<div class="main">
+    		<div class="login">
+    			<div class="login_err">
+    			</div>
+    			<div class="login_user">
+    				<span class="htxt">
+    					帐号：</span>
+    				<span class="l_input">
+    					<input name="user" id="user" placeholder="手机号/邮箱/用户名" type="text">
+    				</span>
+    			</div>
+    			<div class="login_pwd">
+    				<span class="htxt">
+    					密码：
+    				</span>
+    				<span class="l_input">
+    					<input name="pwd" id="pwd" type="password">
+    				</span>
+    			</div>
+    			<div class="login_but">
+    				<a href="javascript:void(0);" id="loginbut" class="loginbut">
+    					登录
+    				</a>
+    				<div class="regfind">
+    					<span>
+    						<a href="http://m.d1.cn/wap/reg.html">
+    							<i class="i1">
+    							</i>
+    							<br>
+    							手机注册
+    						</a>
+    					</span>
+    					<span>
+    						<a href="http://m.d1.cn/wap/regmail.html">
+    							<i class="i2">
+    							</i>
+    							<br>
+    							邮箱注册
+    						</a>
+    					</span>
+    					<span>
+    						<a href="http://m.d1.cn/wap/forgetpwd.jsp">
+    							<i class="i3">
+    							</i>
+    							<br>
+    							找回密码
+    						</a>
+    					</span>   				
+    				</div>
+    			</div>
+    		</div>
+    	</div>
         </div>
     </template>
 
@@ -12,6 +89,178 @@
 
 
     <style scoped>
-
-
+    	*{
+    		margin: 0;
+    		padding: 0;
+    	}
+    	.p_header{
+	    	font-size: 40px;
+	    	color: #555555;
+	    	font-weight: normal;
+	    	text-align: center;
+	    	background: rgb(236, 236, 236);
+	    	overflow: hidden;
+	    }
+	    .p_header .pageback{
+	    	float: left;
+	    	margin: 0.25em 0.1em 0.15em;
+	    	height: 0.666em;
+	    	width: 0.5em;
+	    	background:url(http://images.d1.com.cn/wap/2014/back2.png) no-repeat;	    	
+	    }
+	    .p_header .h_h2{
+	    	text-align: center;
+	    	line-height: 1.2em;	
+	    	float: left;
+	    	width: 2.294rem;
+	    }
+	    .p_header .h_h2 h2{
+	    	font-size: 0.25rem;
+	    	color: #555555;
+	    	font-weight: normal;
+	    }
+	    .p_header .h_h2 h2 i{
+	    	width: 40px;
+	    	height: 40px;
+	    	display: inline-block;
+	    	vertical-align: middle;
+	    	background: url(http://images.d1.com.cn/wap/2014/d1logo.png) no-repeat;
+	    }
+	    .p_header .home{
+	    	float: left;
+	    	background: url(http://images.d1.com.cn/wap/2014/home.png) no-repeat;
+	    	margin: 0.23em 0.1em 0.2em;
+	    	height: 0.59em;
+	    	width: 0.65em;
+	    	background-size: cover;
+	    }
+	    .p_header .myuser{
+	    	float: left;
+	    	background: url(http://images.d1.com.cn/wap/2014/myd1.png) no-repeat;
+	    	margin: 0.23em 0.1em 0.2em;
+	    	height: 0.59em;
+	    	width: 0.65em;
+	    	background-size: cover;
+	    }
+	    .p_header .carth{
+	    	float: left;
+	    	background: url(http://images.d1.com.cn/wap/2014/cart.png) no-repeat;
+	    	margin: 0.23em 0.1em 0.2em;
+	    	height: 0.59em;
+	    	width: 0.65em;
+	    	background-size: cover;
+	    }
+	    .main{
+	    	background: #ffffff;
+	    	font-family: "微软雅黑";
+	    	min-width: 320px;
+	    	margin: 0 auto;
+	    }
+	    .main .login{
+	    	padding: 0.2rem 0.2rem 0 0.2rem;	    	
+	    }
+	    .main .login .login_user{
+	    	vertical-align: middle;
+	    	height: 0.4rem;
+	    	margin: 0.15rem;
+	    	line-height: 0.4rem;
+	    	font-size: 0.18rem;
+	    	border: 1px solid #9f9f9f;
+	    }
+	    .main .login .login_user .htxt{
+	    	display: block;
+	    	padding: 0 0.2px 0 10px;
+	    	font-size: 0.14rem;
+	    	float: left;
+	    }
+	    .main .login .login_user .l_input{
+	    	display: block;
+	    	padding-right: 0.2rem;
+	    	float: left;
+	    }
+	    .main .login .login_user .l_input input{
+	    	border: none;
+	    	font-size: 0.18rem;
+	    	height: 0.35rem;
+	    	width: 100%;
+	    	outline: none;
+	    	color: #666666;
+	    }
+	    .main .login .login_pwd{
+	    	vertical-align: middle;
+	    	height: 0.4rem;
+	    	margin: 0.15rem;
+	    	line-height: 0.4rem;
+	    	font-size: 0.18rem;
+	    	border: 1px solid #9f9f9f;
+	    }
+	    .main .login .login_pwd .htxt{
+	    	display: block;
+	    	padding: 0 0.2px 0 10px;
+	    	font-size: 0.14rem;
+	    	float: left;
+	    }
+	    .main .login .login_pwd .l_input{
+	    	display: block;
+	    	padding-right: 0.2rem;
+	    	float: left;
+	    }
+	    .main .login .login_pwd .l_input input{
+	    	border: none;
+	    	font-size: 0.18rem;
+	    	height: 0.35rem;
+	    	width: 100%;
+	    	outline: none;
+	    	color: #666666;
+	    }
+	    .main .login .login_but{
+	    	padding: 0.1rem;	    	
+	    }
+	    .main .login .login_but .loginbut{
+	    	display: block;
+	    	width: 100%;
+	    	height: 0.36rem;
+	    	background: #f1424f;
+	    	color: #FFFFFF;
+	    	font-size: 0.16rem;
+	    	border-radius: 4px;
+	    	text-align: center;
+	    	line-height: 0.36rem;
+	    }
+	    .main .login .login_but .regfind{
+	    	display: table;
+	    	height: 0.7rem;
+	    	width: 100%;
+	    	line-height: 0.16rem;
+	    	margin-top: 0.1rem;	    	
+	    }
+	    .main .login .login_but .regfind span{
+	    	display: table-cell;
+	    	font-size: 0.14rem;
+	    	text-align: center;
+	    }
+	    .main .login .login_but .regfind span a{
+	    	color: #c61621;
+	    	text-decoration: underline;
+	    	display: inline-block;
+	    	width: 100%;
+	    	height: 100%;
+	    }	    
+	    .main .login .login_but .regfind span a i{
+	    	width: 0.6rem;
+	    	height: 0.5rem;
+	    	display: inline-block;	    	
+	    }
+	    .main .login .login_but .regfind span a .i1{
+	    	background: url(http://images.d1.com.cn/wap/2014/b1.png)  no-repeat;
+	    	background-size: auto 0.4rem;
+	    }
+	    .main .login .login_but .regfind span a .i2{
+	    	background: url(http://images.d1.com.cn/wap/2014/b2.png)  no-repeat;
+	    	background-size: auto 0.4rem;
+	    }
+	    .main .login .login_but .regfind span a .i3{
+	    	background: url(http://images.d1.com.cn/wap/2014/b3.png)  no-repeat;
+	    	background-size: auto 0.4rem;
+	    }	    
     </style>
--- src/components/my.vue
+++ src/components/my.vue
@@ -1,6 +1,121 @@
     <template>
         <div>
-            my
+           <header class="p_header">
+				<div class="h_txt">
+					<div class="pageback">
+						<a href="javascript:window.history.back(-1);">
+						</a>
+					</div>
+					<div class="h_h2">
+						<h2>
+							会员中心
+						</h2>
+					</div>
+				</div>
+			</header>
+           <div class="main">
+				<div class="umenu">
+					<div class="um_top">
+						<i>
+						</i>
+						<div class="txt">
+							<span id="muid">
+								15634203324
+							</span>
+							<span id="mtype">
+								普通会员
+							</span>
+							<span id="mscore">
+								0积分
+							</span>
+						</div>
+					</div>
+					<div class="um_btm">
+						<div>
+							<ul class="mitem">
+								<li class="bg1">
+									<a href="user_orderlist.html">
+										<i>
+										</i>
+										<span>
+											我的订单
+										</span>
+									</a>
+								</li>
+								<li class="bg2">
+									<a href="user_mytkts.html">
+										<i>
+										</i>
+										<span>
+											我的优惠券
+										</span>
+									</a>
+								</li>
+								<li class="bg3">
+									<a href="user_myfav.html">
+										<i>
+										</i>
+										<span>
+											我的收藏
+										</span>
+									</a>
+								</li>
+							</ul>
+							<ul class="mitem">
+								<li class="bg4">
+									<a href="user_myscore.html">
+										<i>
+										</i>
+										<span>
+											我的积分
+										</span>
+									</a>
+								</li>
+								<li class="bg5">
+									<a href="user_mpwd.html">
+										<i>
+										</i>
+										<span>
+											修改密码
+										</span>
+									</a>
+								</li>
+								<li class="bg4">
+									<a href="score.html">
+										<i>
+										</i>
+										<span>
+											积分商城
+										</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+        	<div id="footer" class="footer">
+				<div class="ftxt">
+					<span class="user">
+						<a href="login.html" class="l">
+							请登陆
+						</a>
+						<a href="wxtt.jsp?backurl=/wap/reg.html" class="r">
+							注册
+						</a>
+					</span>
+					<span class="top">
+						<a href="#top">
+							回到顶部
+						</a>
+					</span>
+				</div>
+				<div class="fcopy">
+					客服热线:400-108-5518(工作日9:00-18:00)
+					<br>
+					Copyright ©2017 京030072
+				</div>
+			</div>
         </div>
     </template>
 
@@ -10,8 +125,200 @@
         }
     </script>
 
-
+ 
     <style scoped>
+	.p_header, .p_header input, .p_header button {
+    font-size: 40px;
+}
+.p_header .h_txt {
+    background-color: #ececec;
+    -webkit-box-shadow: 0 2px 4px #d9d9d9;
+    box-shadow: 0 2px 4px #d9d9d9;
+    height: 1.2em;
+    font-size: 1em;
+    min-width: 320px;
+    display: -webkit-box;
+    overflow: hidden;
+}
+.p_header .h_txt .pageback {
+    margin: .25em .10em 0 .15em;
+    height: 0.666em;
+    width: .50em;
+    background: url(http://images.d1.com.cn/wap/2014/back2.png) no-repeat;}
+ h2 {
+    display: block;
+    font-size: 1.5em;
+    font-weight: normal;
+    font-size: 0.25rem;
+}   
+.p_header .h_txt .h_h2 {
+    font-size: .25rem;
+    text-align: center;
+    -webkit-box-flex: 1;
+    flex: 1;
+    line-height: 1.6em;
+}
+
+/*main*/
+.main {
+    background-color: #fff;
+    font-family: 微软雅黑;
+    min-width: 320px;
+    margin: .10rem auto;
+    font-size: .14rem;
+    max-width: 640px;
+}
+.main .umenu {
+    background: #f6f6f6;
+    padding: .10rem 0;
+    border-bottom: 1px solid #a5a5a5;
+}
+.main .umenu .um_top {
+    height: 1.00rem;
+    display: -webkit-box;
+    width: 90%;
+    overflow: hidden;
+    padding-right: 10%;
+    font-size: .14rem;
+}
+.main .umenu .um_btm .mitem li i {
+    width: 100%;
+    height: 2.5em;
+    display: block;
+}
+.main .umenu .um_top i {
+    padding-left: .10rem;
+    width: .70rem;
+    height: 1.00rem;
+    display: block;
+    background: url(http://images.d1.com.cn/wap/2014/ubg.png) center center no-repeat;
+}
+.mitem li.bg2 i {
+    background: url(http://images.d1.com.cn/wap/2014/02.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+.mitem li.bg3 i {
+    background: url(http://images.d1.com.cn/wap/2014/03.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+.mitem li.bg4 i {
+    background: url(http://images.d1.com.cn/wap/2014/04.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+.main .umenu .um_btm .mitem li.bg5 i {
+    background: url(http://images.d1.com.cn/wap/2014/05.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+.main .umenu .um_btm .mitem li.bg4 i {
+    background: url(http://images.d1.com.cn/wap/2014/04.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+.main .umenu .um_top .txt {
+    -webkit-box-flex: 1;
+    flex: 1;
+    line-height: .28rem;
+    overflow: hidden;
+}
+.main .umenu .um_top .txt>span {
+    width: 100%;
+    height: .30rem;
+    display: block;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    -webkit-text-overflow: ellipsis;
+}
+span{color: #666;}
+div {
+    display: block;
+}
+.main .umenu .um_btm .mitem {
+    height: .4em;
+    display: table;
+    width: 100%;
+    padding-top: .10rem;
+}
+base.css:6
+ul, ol {
+    list-style: none;
+}
+.main .umenu .um_btm .mitem li {
+    display: table-cell;
+    text-align: center;
+    width: 33.33%;
+}
+.main .umenu .um_btm .mitem {
+    height: .4em;
+    display: table;
+    width: 100%;
+    padding-top: .10rem;
+}
+.main .umenu .um_btm .mitem a{text-decoration: none;}
+ul, ol {
+    list-style: none;
+}
+/*.bg1{background: url(http://images.d1.com.cn/wap/2014/01.png) center center no-repeat;}*/
+.main .umenu .um_btm .mitem li.bg1 i {
+    background: url(http://images.d1.com.cn/wap/2014/01.png) center center no-repeat;
+    background-size: 2.3em 2.3em;
+}
+/*footer*/
+.footer {
+    background-color: #ececec;
+    -webkit-box-shadow: 0 -2px 4px #d9d9d9;
+    box-shadow: 0 -2px 4px #d9d9d9;
+    margin-bottom: 0.60rem;
+    font-size: 0.16rem;
+}
+.footer .user {
+    padding-left: 0.20rem;
+    color: #555555;
+    line-height: 0.70rem;
+    width: 2.40rem;
+    display: block;
+}
+.footer .user a.l {
+    width: 1.4rem;
+}
+.footer .user a {
+	text-decoration: none;
+    display: inline-block;
+    color: #666;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+}
+.main .umenu .um_btm .mitem li a {
+    width: 100%;
+    display: block;
+}
+.footer .top {
+    height: 0.70rem;
+    line-height: 0.70rem;
+    fax: 1;
+    -webkit-box-flex: 1;
+    text-align: right;
+    padding-right: .30rem;
+    display: block;
+}
+.footer .top a{text-decoration: none;}
+.footer .ftxt {
+    height: 0.70rem;
+    width: 100%;
+    display: -webkit-box;
+}
+.footer .top a {
+    color: #666;
+    display: block;
+}
+.footer .fcopy {
+    font-size: .14rem;
+    color: #666;
+    text-align: center;
+    width: 100%;
+    height: .50rem;
+    line-height: .23rem;
+}
 
 
     </style>
